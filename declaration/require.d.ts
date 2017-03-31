e309af624c596a35e2c0f9aa30ec8fb8ed37a79f
placeless <41974873@qq.com>
2017-03-31 22:41:54 +0800

    1231

--- declaration/require.d.ts
+++ declaration/require.d.ts
@@ -1,7 +1,7 @@
 /**
  * Created by placeless on 17/3/30.
  */
-declare var require: {
+declare const require: {
     <T>(path: string): T;
     (paths: string[], callback: (...modules: any[]) => void): void;
     ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
